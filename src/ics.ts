79f1d79c093c71086cc316661072248d5cf46efd
Kurt Medley <kmedley@rei.com>
2023-08-18 11:42:33 -0700

    testing description

--- src/ics.ts
+++ src/ics.ts
@@ -36,16 +36,16 @@ const logger = LoggerFactory({ level: 'info' });
       ],
       title,
     };
-    event.description = '';
-    if (region) {
-      event.description += `Region: ${region}\n`;
-    }
-    if (location) {
-      event.description += `Location: ${location}\n`;
-    }
-    if (moreInfo.oilPattern) {
-      event.description += `Oil pattern: ${moreInfo.oilPattern}\n`;
-    }
+    event.description = 'Region: test region\nLocation: test location';
+    // if (region) {
+    //   event.description += `Region: ${region}\n`;
+    // }
+    // if (location) {
+    //   event.description += `Location: ${location}\n`;
+    // }
+    // if (moreInfo.oilPattern) {
+    //   event.description += `Oil pattern: ${moreInfo.oilPattern}\n`;
+    // }
     if (moreInfo.link) {
       event.url = moreInfo.link;
       // event.description += `More info: ${moreInfo.link}\n`;
